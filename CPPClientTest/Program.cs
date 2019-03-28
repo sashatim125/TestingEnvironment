c0b4f8e2bab3f606b825bda259ab9b6bd4251018
sashatim125 <alexander@ravendb.net>
2019-03-28 17:53:34 +0200

    update

---
+++ CPPClientTest/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CPPClientTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RunCppClientTest(args[0]);
+        }
+
+        static void RunCppClientTest(string orchestratorUrl)
+        {
+            using (var client = new Test(orchestratorUrl, "CppClientTest"))
+            {
+                client.Initialize();
+                client.RunTest();
+            }
+
+        }
+    }
+}
---
+++ CPPClientTest/Test.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Runtime.InteropServices;
+using TestingEnvironment.Client;
+
+namespace CPPClientTest
+{
+    class Test : BaseTest
+    {
+        public Test(string orchestratorUrl, string testName) : base(orchestratorUrl, testName, "Alexander")
+        {
+        }
+
+        [DllImport(@"test4.2.dll", EntryPoint = "run_tests")]
+        private static extern unsafe void run_tests(
+            string[] urls,
+            Int32 numOfUrls,
+            string dbName,
+            void* resultBuffer,
+            out Int32 resultBufferLength);
+
+        private unsafe void RunMyTest()
+        {
+            var resultBuffer = new byte[1024 * 1024];
+            Int32 resultBufferLength;
+
+            ReportInfo("Cpp client test started");
+
+            fixed (byte* buffer = resultBuffer)
+            {
+                run_tests(DocumentStore.Urls, DocumentStore.Urls.Length,
+                    DocumentStore.Database,
+                    buffer, out resultBufferLength);
+            }
+
+            var testResult = Encoding.UTF8.GetString(resultBuffer, 0, resultBufferLength);
+            ReportInfo(testResult);
+
+            ReportInfo("Cpp client test finished");
+        }
+
+        public override void RunActualTest()
+        {
+            RunMyTest();
+        }
+    }
+}
+
